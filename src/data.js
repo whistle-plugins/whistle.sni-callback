c8d708e539743261e5b99bbcb92c9bf35953fb12
avwo <avwu@qq.com>
2021-10-08 15:35:57 +0800

    feat: add cert cgi

--- src/data.js
+++ src/data.js
@@ -1,3 +1,36 @@
+const JSON_TYPE = { 'Content-Type': 'application/json' };
+
+function getTimeoutPromise() {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error('timeout'));
+    }, 10000);
+  });
+}
+
+function toJson(res) {
+  return res.json().then((data) => {
+    if (data.retcode !== 0) {
+      const err = new Error(data.retmsg || '未知错误');
+      err.retcode = data.retcode;
+      throw err;
+    }
+    return data;
+  });
+}
+
+function fetchData(url, data) {
+  return Promise.race([
+    fetch(url, {
+      method: data == null ? 'GET' : 'POST',
+      cache: 'no-cache',
+      headers: JSON_TYPE,
+      body: data && JSON.stringify(data),
+    }),
+    getTimeoutPromise(),
+  ]).then(toJson);
+}
+
 export const uploadCerts = () => {};
 export const getCertsInfo = () => {};
 export const removeCert = () => {};
